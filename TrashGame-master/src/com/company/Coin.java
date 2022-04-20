154d6861982794cd2090087b2729f64b2719b06e
ItamarAtia <atia49@gmail.com>
2022-04-20 13:01:25 +0300

    itamar test

--- TrashGame-master/src/com/company/Coin.java
+++ TrashGame-master/src/com/company/Coin.java
@@ -1,2 +1,20 @@
-package com.company;public class Coin {
+package com.company;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+public class Coin extends JLabel {
+
+    private ImageIcon coin;
+    private int value;
+    //todo:not sure about speed field
+    private int speed;
+    public Coin(int x,int y,int width,int height,ImageIcon coin,int value,int speed) {
+        this.setBounds(x,y,width,height);
+        this.coin = coin;
+        this.value=value;
+        this.speed=speed;
+        this.setOpaque(true);
+    }
+
 }
+
